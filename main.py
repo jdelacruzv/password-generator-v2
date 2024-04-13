d0a5c4abb742b6a7c58e627f43ebd52e5eee6b36
Jose De La Cruz <jldlcv@gmail.com>
2024-04-13 22:27:08 +0200

    Removed the last three lines of code

--- main.py
+++ main.py
@@ -27,7 +27,3 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
 
 # new_password = generate_password(8)
 # print(new_password)
-
-pattern = r'\.'
-quote = 'Not all those who wander are lost.'
-# print(re.findall(pattern, quote))
